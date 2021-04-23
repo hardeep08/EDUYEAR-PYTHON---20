f92566c241a7c474146fd0874b019fca13a529ca
hardeep08 <77842550+hardeep08@users.noreply.github.com>
2021-04-23 15:29:03 +0530

    Add files via upload

--- Day 2.py
+++ Day 2.py
@@ -1,4 +1,6 @@
-a = 10
-b = "hello world!!"
-c = 28
-print(a, b, c)
+accno = 53718
+name = "Hardeep"
+branch = "Sbi ambala"
+amount = 10500.85
+
+print("Mr {}, Debited Rs.{}, from account number {}, of branch {}.".format(name , amount, accno, branch))
